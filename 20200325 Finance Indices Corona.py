ffaa11062204ea04b73a126282663c26e557b512
r357 <alenrozac@gmail.com>
2020-05-18 11:12:16 +0200

    upd

--- 20200325 Finance Indices Corona.py
+++ 20200325 Finance Indices Corona.py
@@ -2,16 +2,19 @@ import matplotlib.pyplot as plt
 from functools import reduce
 import pandas as pd
 import yfinance as yf
+import os
 
-indices = ["^GSPC", "^HSI", "^FTSE", "^SSEC", "^N225", "^GDAXI"]
-data = [pd.DataFrame(yf.Ticker(i).history(start="2020-2-21")) for i in indices]
+# indices = ["^GSPC", "^HSI", "^FTSE", "^SSEC", "^N225", "^GDAXI"]
+indices = ["^VIX", "^TNX", "^IRX", "^FVX", "LQD"]
+data = [pd.DataFrame(yf.Ticker(i).history(start="2008-9-15", end="2008-12-12")) for i in indices]
 
 rets, closes = pd.DataFrame(), pd.DataFrame()
 
 for i, df in enumerate(data):
     rets[i] = df["Close"].pct_change()
     closes[i] = df["Close"]/df["Close"][0]
-    
+    df.to_excel(os.getcwd()+"/Downloaded/"+indices[i].replace("^","")+".xls")
+	    
 rets.columns, closes.columns = indices, indices
 rets.plot()
 closes.plot()
