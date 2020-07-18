be1f4e1a634e92e0aa1f3785eab48a40f5f7926a
r357 <alenrozac@gmail.com>
2020-07-18 22:24:34 +0200

    whatever

---
+++ fibonacci.py
@@ -0,0 +1,51 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+# Fibonacci
+"""
+
+Sešteva zadnje 2 cifre
+1,1,2,3,5
+1+1
+2+1
+3+2
+5+3
+8+5
+...
+
+"""
+
+#za naslednjih n mest dobi fibn
+n = 50
+
+"""
+fib(1) = 1
+fib(2) = 1
+fib(3) = 2
+fib(4) = 3
+fib(5) = 5
+"""
+
+def fib(n, omeitev=None):
+	zaporedje = [1, 1]
+
+	# začetek zaporedja
+	cifra1 = 
+	cifra2 = 1
+	naslednja_cifra_po_fib_zap = cifra1 + cifra2
+
+	# optional omejitev
+	if omeitev:
+
+
+	# spravi naslednjo cifro na seznam
+	zaporedje.append(s3)
+
+	# zaporedje z dolzino n
+
+	return zaporedje
+
+
+
+a = fib(50)
+
