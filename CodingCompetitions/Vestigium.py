e64cb79e36465e42fbc7cb1cc01ce7c7352b0a87
r357 <alenrozac@gmail.com>
2020-10-30 20:57:39 +0100

    upd

---
+++ CodingCompetitions/Vestigium.py
@@ -0,0 +1,61 @@
+''' Vestigium = trace.
+
+NxN matrix, N different values, no value repeated in row or col.
+1:N
+
+- We want to compute trace
+- compute number of rows and cols that contain repeated values
+
+'''
+
+import numpy as np 
+
+
+def Vestigium (input):
+	'''
+	T * NxN(M_i,j)
+
+	T ... Number of test cases t; Every t starts with a
+	N ... Size of the matrix to explore. N lines follow.
+	i ... row
+	j ... col
+	
+	R: case #x: k r c
+	x = t
+	k ... trace of matrix
+	r ... num of rows of the matrix that contain rep. elements
+	c ... num of cols of the matrix that contain rep. el.
+	'''
+
+	lambda
+
+
+
+
+	pass
+
+
+
+inp = '''3
+4
+1 2 3 4
+2 1 4 3
+3 4 1 2
+4 3 2 1
+4
+2 2 2 2
+2 3 2 3
+2 2 2 3
+2 2 2 2
+3
+2 1 3
+1 3 2
+1 2 3'''
+
+
+Vestigium(inp.split("\n"))
+
+
+
+
+
